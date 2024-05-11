1ec012d682a23cd0f296c01350028e0ed01a43cb
Your Name <you@example.com>
2024-05-11 13:09:00 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+// let a =  ("12");
+
+// alert(a)
+
+// alert(typeof(a))
+
+
+
+
+
+// let a = +prompt("birinchi soni kiriting");
+// let b = +prompt("ikkinchi soni kiriting")
+// let c;
+
+
+// function hisob() {
+//     c = b + a
+//     console.log(c);
+// }
+
+// hisob();
+
+
+
+
+// let a = Math.random
+// let c;
+// function abc() {
+//     if (a % 2 === 0) {
+//       console.log("bu son juft " + a);  
+//     }else {
+//         console.log("bu son toq" + a);
+//     }
+// }
+
+// abc();
+
+
+// let a = prompt("ihtiyoriy soz kiriting");
+// let c = "mars IT school"
+// function asd() {
+//     if (a === "mars") {
+//         console.log(a);
+//     }else{
+//         console.log(c);
+//     }
+// }
+
+// asd();
+
+
+
+// let array = ["olma", "banan", "anor", "nok", "shoftoli"];
+// let c = [array];
+
+// function abs() {
+//     for (let i = 0; i > array.length; i++) {
+//         if (array[a] > 5) {
+//             c.push(array[i])
+//         }
+//     }
+//     console.log(c);
+// }
+
+// abs();
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+//     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+//     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+//     31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+//     41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+//     51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+//     61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+//     71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+//     81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+// 91, 92, 93, 94, 95, 96, 97, 98, 99, 100,("aralash son")];
+// console.log(array);
+
+
+// for (let i = 0; i < array.length; i++) {
+//     let check = array[i] % 2
+//     if (check === 0) {
+//         console.log(array[i], ("juft son"));
+//     } else { console.log(array[i], ("toq son")); }
+// }
+
